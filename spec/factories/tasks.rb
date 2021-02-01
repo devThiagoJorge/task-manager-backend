9d3f9161a918b2490d597b50ab1373e03da1f344
Thiago Vinicius Jorge <thiago.jorge@programmers.com.br>
2021-02-01 13:32:51 -0300

    adding action index to tasks

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
     factory :task do
         title { Faker::Lorem.sentence }
-        title { Faker::Lorem.paragraph }
-        deadline { Fake::Date.forward } 
+        description { Faker::Lorem.paragraph }
+        deadline { Faker::Date.forward } 
         done false
         user_id "1"
         user
---
+++ spec/requests/api/v1/tasks_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Task API', type: :request do
+
+    let!(:user) { create(:user) } 
+    let(:headers)  do
+        {
+            'Accept' => 'application/api.taskmanager.v1',
+            'Content-Type' => Mime[:json].to_s, 
+            'Authorization' => user.auth_token
+        }
+    end
+    
+    before { host! 'api.taskmanager.test' }
+
+    describe 'GET /tasks' do
+      before do
+        create_list(:task, 5, user_id: user.id)
+        get '/tasks', params: {}, headers: headers
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns 5 tasks to database' do
+          expect(json_body[:tasks].count).to eq(5)
+      end
+    end
+   
+    
+end
